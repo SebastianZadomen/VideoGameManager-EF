3a0dfa09d902d05f00fc3570682f9b685427a9d0
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-05-06 22:01:08 +0200

    DbContext created based on the models, clsoes #7

---
+++ VideoGameManager/wwwroot/Data/GameStoreContext.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoGameManager.wwwroot.Data
+{
+    public class GameStoreContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+
+        public GameStoreContext(DbContextOptions<GameStoreContext> options)
+            : base(options) { }
+    }
+
+}
