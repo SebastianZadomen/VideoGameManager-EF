aef4169b6b65fbe3554dfe3c9760029240a30eed
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-05-07 21:36:16 +0200

    The index page was updated to display and receive data from the database. , closes #13

--- VideoGameManager/Pages/Games/Index.cshtml.cs
+++ VideoGameManager/Pages/Games/Index.cshtml.cs
@@ -20,7 +20,42 @@ namespace VideoGameManager.Pages.Games
         public async Task OnGetAsync()
         {
 
-          
+            Games = _context.GetAll();
+            if (!_context.Developers.Any())
+            {
+                var dev1 = new Developer { Name = "Nintendo", Country = "Japan", FoundedYear = 1889 };
+                var dev2 = new Developer { Name = "CD Projekt Red", Country = "Poland", FoundedYear = 1994 };
+                _context.Developers.AddRange(dev1, dev2);
+                _context.SaveChanges();
+
+                _context.Games.AddRange(
+                    new Game
+                    {
+                        Title = "The Legend of Zelda: TotK",
+                        Genre = "Adventure",
+                        Year = 2023,
+                        Score = 9.8,
+                        DeveloperId = dev1.Id
+                    },
+                    new Game
+                    {
+                        Title = "Mario Kart 8",
+                        Genre = "Racing",
+                        Year = 2014,
+                        Score = 8.7,
+                        DeveloperId = dev1.Id
+                    },
+                    new Game
+                    {
+                        Title = "The Witcher 3",
+                        Genre = "RPG",
+                        Year = 2015,
+                        Score = 9.5,
+                        DeveloperId = dev2.Id
+                    }
+                );
+                _context.SaveChanges();
+            }
 
         }
 
